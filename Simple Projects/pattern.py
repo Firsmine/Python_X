9d660de0d04e63ce55758999b7b003e97fa43cea
firsmine <sitishafira.fia@gmail.com>
2026-08-08 22:15:02 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -32,3 +32,14 @@ def butterfly(n):
         print(" ", end="")
     print()
 print(butterfly(5))
+
+print("\nHollow Square")
+def hollow_square(n):
+  for i in range(n):
+    for j in range(n):
+      if i == 0 or i == n-1 or j == 0 or j == n-1:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
+print(hollow_square(5))
