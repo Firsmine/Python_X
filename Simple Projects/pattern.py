0bef7bcb0d57f739c7e6de3b048e9d422044021d
firsmine <sitishafira.fia@gmail.com>
2026-08-08 22:10:58 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -26,4 +26,9 @@ def butterfly(n):
   for i in range(2 * n - 1):
     for j in range(2 * n - 1):
       h = i if i < n else 2 * n - 2 - i
-      
+      if j <= h or j >= 2 * n - 2 - h:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
+print(butterfly(5))
