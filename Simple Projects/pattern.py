82b0daf1921f5df8d3d8f50f9fde76c27a462c3d
firsmine <sitishafira.fia@gmail.com>
2026-08-09 21:41:11 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -6,54 +6,47 @@ print("4. Hollow Square")
 
 pattern = int(input("Choose A Pattern You Wanna Make (1-4): "))
 
-rows = int(input("The row size pattern: "))
-for i in range(1, rows + 1):
-  for j in range(1, i + 1):
-    print("*", end=" ")
-  print()
-
-print("\nDiamond")
-def diamond(n):
-  for i in range(2 * n - 1):
-    for j in range(2 * n - 1):
-      h = i if i < n else 2 * n -2 - i
-      if n - 1 - h <= j <= n - 1 + h:
-        print("*", end="")
-      else:
-        print(" ", end="")
+if pattern == 1:
+  rows = int(input("The row size pattern: "))
+  for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+      print("*", end=" ")
     print()
-print(diamond(5))
+    
+elif pattern == 2:
+  print("\nDiamond")
+  def diamond(n):
+    for i in range(2 * n - 1):
+      for j in range(2 * n - 1):
+        h = i if i < n else 2 * n -2 - i
+        if n - 1 - h <= j <= n - 1 + h:
+          print("*", end="")
+        else:
+          print(" ", end="")
+      print()
+  print(diamond(5))
 
-print("\nRight Triangle")
-def rightTriangle(n):
-  for i in range(n):
-    for j in range(n):
-      if j <= i:
-        print("*", end="")
-      else:
-        print(" ", end="")
-    print()
-print(rightTriangle(5))
+elif pattern == 3:
+  print("\nButterfly")
+  def butterfly(n):
+    for i in range(2 * n - 1):
+      for j in range(2 * n - 1):
+        h = i if i < n else 2 * n - 2 - i
+        if j <= h or j >= 2 * n - 2 - h:
+          print("*", end="")
+        else:
+          print(" ", end="")
+      print()
+  print(butterfly(5))
 
-print("\nButterfly")
-def butterfly(n):
-  for i in range(2 * n - 1):
-    for j in range(2 * n - 1):
-      h = i if i < n else 2 * n - 2 - i
-      if j <= h or j >= 2 * n - 2 - h:
-        print("*", end="")
-      else:
-        print(" ", end="")
-    print()
-print(butterfly(5))
-
-print("\nHollow Square")
-def hollow_square(n):
-  for i in range(n):
-    for j in range(n):
-      if i == 0 or i == n-1 or j == 0 or j == n-1:
-        print("*", end="")
-      else:
-        print(" ", end="")
-    print()
-print(hollow_square(5))
+elif pattern == 4:
+  print("\nHollow Square")
+  def hollow_square(n):
+    for i in range(n):
+      for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+          print("*", end="")
+        else:
+          print(" ", end="")
+      print()
+  print(hollow_square(5))
