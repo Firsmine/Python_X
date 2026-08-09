387a8909ec3548fc2649889ba907a816758d01ee
firsmine <sitishafira.fia@gmail.com>
2026-08-09 21:45:36 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -40,12 +40,11 @@ elif pattern == 3:
 
 elif pattern == 4:
   print("\nHollow Square")
-  def hollow_square(n):
-    for i in range(n):
-      for j in range(n):
-        if i == 0 or i == n-1 or j == 0 or j == n-1:
-          print("*", end="")
-        else:
-          print(" ", end="")
-      print()
-  print(hollow_square(5))
+  n = int(input("The row size pattern: "))
+  for i in range(n):
+    for j in range(n):
+      if i == 0 or i == n-1 or j == 0 or j == n-1:
+        print("*", end=" ")
+      else:
+        print(" ", end=" ")
+    print()
