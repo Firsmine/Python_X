e210b5aeeef350a7d159953e644661b30a602176
firsmine <sitishafira.fia@gmail.com>
2026-08-09 21:43:27 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -15,16 +15,15 @@ if pattern == 1:
     
 elif pattern == 2:
   print("\nDiamond")
-  def diamond(n):
-    for i in range(2 * n - 1):
-      for j in range(2 * n - 1):
-        h = i if i < n else 2 * n -2 - i
-        if n - 1 - h <= j <= n - 1 + h:
-          print("*", end="")
-        else:
-          print(" ", end="")
-      print()
-  print(diamond(5))
+  n = int(input("The row size pattern: "))
+  for i in range(2 * n - 1):
+    for j in range(2 * n - 1):
+      h = i if i < n else 2 * n -2 - i
+      if n - 1 - h <= j <= n - 1 + h:
+        print("*", end=" ")
+      else:
+        print(" ", end=" ")
+    print()
 
 elif pattern == 3:
   print("\nButterfly")
