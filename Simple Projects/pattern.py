ce6a68795dcc7071247c8786893205ce40da677f
firsmine <sitishafira.fia@gmail.com>
2026-08-10 21:02:47 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -1,50 +1,54 @@
-print("\n===== Make Pattern =====")
-print("1. Right Triangle")
-print("2. Diamond")
-print("3. Butterfly")
-print("4. Hollow Square")
+while True:
+  print("\n===== Make Pattern =====")
+  print("1. Right Triangle")
+  print("2. Diamond")
+  print("3. Butterfly")
+  print("4. Hollow Square")
 
-pattern = int(input("Choose A Pattern You Wanna Make (1-4): "))
+  pattern = int(input("Choose A Pattern You Wanna Make (1-4): "))
 
-if pattern == 1:
-  print("\nRight Triangle")
-  rows = int(input("The row size pattern: "))
-  for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-      print("*", end=" ")
-    print()
-    
-elif pattern == 2:
-  print("\nDiamond")
-  n = int(input("The row size pattern: "))
-  for i in range(2 * n - 1):
-    for j in range(2 * n - 1):
-      h = i if i < n else 2 * n -2 - i
-      if n - 1 - h <= j <= n - 1 + h:
+  if pattern == 1:
+    print("\nRight Triangle")
+    rows = int(input("The row size pattern: "))
+    for i in range(1, rows + 1):
+      for j in range(1, i + 1):
         print("*", end=" ")
-      else:
-        print(" ", end=" ")
-    print()
+      print()
+    if input("Try another pattern? (y/n): ") != "y": 
+      print("G'bye!")
+      break
+      
+  elif pattern == 2:
+    print("\nDiamond")
+    n = int(input("The row size pattern: "))
+    for i in range(2 * n - 1):
+      for j in range(2 * n - 1):
+        h = i if i < n else 2 * n -2 - i
+        if n - 1 - h <= j <= n - 1 + h:
+          print("*", end=" ")
+        else:
+          print(" ", end=" ")
+      print()
 
-elif pattern == 3:
-  print("\nButterfly")
-  n = int(input("The row size pattern: "))
-  for i in range(2 * n - 1):
-    for j in range(2 * n - 1):
-      h = i if i < n else 2 * n - 2 - i
-      if j <= h or j >= 2 * n - 2 - h:
-        print("*", end=" ")
-      else:
-        print(" ", end=" ")
-    print()
+  elif pattern == 3:
+    print("\nButterfly")
+    n = int(input("The row size pattern: "))
+    for i in range(2 * n - 1):
+      for j in range(2 * n - 1):
+        h = i if i < n else 2 * n - 2 - i
+        if j <= h or j >= 2 * n - 2 - h:
+          print("*", end=" ")
+        else:
+          print(" ", end=" ")
+      print()
 
-elif pattern == 4:
-  print("\nHollow Square")
-  n = int(input("The row size pattern: "))
-  for i in range(n):
-    for j in range(n):
-      if i == 0 or i == n-1 or j == 0 or j == n-1:
-        print("*", end=" ")
-      else:
-        print(" ", end=" ")
-    print()
+  elif pattern == 4:
+    print("\nHollow Square")
+    n = int(input("The row size pattern: "))
+    for i in range(n):
+      for j in range(n):
+        if i == 0 or i == n-1 or j == 0 or j == n-1:
+          print("*", end=" ")
+        else:
+          print(" ", end=" ")
+      print()
