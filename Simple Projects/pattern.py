2af69efdea210669b4d74f7af3e000f3c8cc558c
firsmine <sitishafira.fia@gmail.com>
2026-08-10 21:13:30 +0800

    5

--- Simple Projects/pattern.py
+++ Simple Projects/pattern.py
@@ -13,7 +13,11 @@ while True:
 
   if pattern == 1:
     print("\nRight Triangle")
-    n = int(input("The row size pattern: "))
+    try:
+      n = int(input("The row size pattern: "))
+    except ValueError:
+      print("\n>>> Please input the valid number.")
+      continue
     for i in range(1, n + 1):
       for j in range(1, i + 1):
         print("*", end=" ")
@@ -24,7 +28,11 @@ while True:
       
   elif pattern == 2:
     print("\nDiamond")
-    n = int(input("The row size pattern: "))
+    try:
+      n = int(input("The row size pattern: "))
+    except ValueError:
+      print("\n>>> Please input the valid number.")
+      continue
     for i in range(2 * n - 1):
       for j in range(2 * n - 1):
         h = i if i < n else 2 * n -2 - i
